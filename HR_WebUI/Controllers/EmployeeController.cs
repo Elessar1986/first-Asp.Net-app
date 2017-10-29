b7865cce71137b0c428da4e87db0478523e9ab48
Elessar <ivan.arepjev@gmail.com>
2017-10-29 21:29:29 +0200

    Добавьте файлы проекта.

---
+++ HR_WebUI/Controllers/EmployeeController.cs
@@ -0,0 +1,62 @@
+﻿using HR_DAL.DbLayer;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HR_WebUI.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        HrContext context;
+
+        public EmployeeController()
+        {
+            context = new HrContext();
+        }
+        public ActionResult Index()
+        {
+            var model = context.Employees;
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id = 0)
+        {
+            var model = (id == 0) ? new Employee() : context.Employees.Find(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Employee obj)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Employees.AddOrUpdate(obj);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var emp = context.Employees.Find(id);
+            if(emp != null)
+            {
+                context.Employees.Remove(emp);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            context.Dispose();
+        }
+    }
+}
---
+++ HR_WebUI/Controllers/JobTitleController.cs
@@ -0,0 +1,70 @@
+﻿using HR_DAL.DbLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity.Migrations;
+
+namespace HR_WebUI.Controllers
+{
+    public class JobTitleController : Controller
+    {
+        // GET: JobTitle
+
+        HrContext context;
+
+        public JobTitleController()
+        {
+            context = new HrContext();
+        }
+        public ActionResult Index()
+        {
+            var model = context.JobTitles;
+            return View(model);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var job = context.JobTitles.Find(id);
+            if (job != null)
+            {
+                context.JobTitles.Remove(job);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id = 0)
+        {
+            var model = (id == 0) ? new JobTitle() : context.JobTitles.Find(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(JobTitle obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                context.JobTitles.AddOrUpdate(obj);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            Dispose_();
+        }
+        public void Dispose_()
+        {
+            context.Dispose();
+        }
+
+    }
+}
