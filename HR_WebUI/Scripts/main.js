b5327586a0200d022465bad91a76d81d4016798f
Elessar <ivan.arepjev@gmail.com>
2017-11-03 22:44:21 +0200

    с дизайном все ок все работает

---
+++ HR_WebUI/Scripts/main.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+
+    console.log("loaded");
+
+    $(".deleteEmployee").click(function () {
+        if (confirm("You are sure?")) {
+            return true;
+        }
+        else {
+            e.preventDefault();
+        }
+    })
+})
